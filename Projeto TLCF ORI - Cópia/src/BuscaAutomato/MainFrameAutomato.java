3f84e260a8ae1f790bb88902a750036451c0210d
Guilherme Ricarte <guiricarte@gmail.com>
2013-01-08 12:14:14 -0200

    Revert "FINAL COMPLETO FUNCIONAMENTO OK"
    This reverts commit 89534c5968e4f539dff455b2228bfec89e66c1ca.

---
+++ Projeto TLCF ORI - Cópia/src/BuscaAutomato/MainFrameAutomato.java
@@ -0,0 +1,189 @@
+package BuscaAutomato;
+
+import BuscaAutomato.UploadFiles.UploadFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+public class MainFrameAutomato extends JFrame implements ActionListener
+{
+    private JTextField tftxcampoBusca;
+    private JLabel lblCampoBusca, lbl_result, lblCaminho;
+    private JButton btnBusca, btnUpload, btnresultV,btnresultI, btnresultA;
+    private String textoBusca, tipo;
+    private String[] textos;
+    private int i;
+    private String retornoBusca, caminhoFinal;
+    private String caminhoarquivo = "Files/search.png";
+    ImageIcon img_result;
+    
+            Runtime processo;
+        String linhaComando;
+    public MainFrameAutomato()
+    {
+        setBounds (350,300,700,180);
+        setTitle("Pesquisa | Projeto de ORI + TCLF");
+        setResizable(false);
+        getContentPane().setLayout(null);
+        
+        img_result = new ImageIcon(caminhoarquivo);
+        lbl_result = new JLabel(img_result);
+        lbl_result.setBounds(200,15,520,200);
+        lbl_result.setLayout(null);
+        
+        lblCampoBusca = new JLabel();
+        lblCampoBusca.setText("Buscar:");
+        lblCampoBusca.setBounds(05,20,320,25);
+        
+        lblCaminho = new JLabel();
+        lblCaminho.setText("Caminho: ");
+        lblCaminho.setBounds(05,50,320,25);
+        
+        btnresultV = new JButton();
+        btnresultV.setText("Video");
+        btnresultV.setBounds(535,50,150,25);
+        btnresultV.addActionListener(this);
+        
+        btnresultA = new JButton();
+        btnresultA.setText("Audio");
+        btnresultA.setBounds(535,80,150,25);
+        btnresultA.addActionListener(this);
+        
+        btnresultI = new JButton();
+        btnresultI.setText("Imagem");
+        btnresultI.setBounds(535,110,150,25);
+        btnresultI.addActionListener(this);
+        
+        tftxcampoBusca = new JTextField("O que deseja procurar?");
+        tftxcampoBusca.setBounds(60,20,405,25);
+                
+        btnBusca = new JButton("Ir");
+        btnBusca.setBounds(475,20,50,25);
+        btnBusca.addActionListener(this);
+        
+        btnUpload = new JButton("Enviar Arquivos");
+        btnUpload.setBounds(535,20,150,25);
+        btnUpload.addActionListener(this);
+        
+        
+        getContentPane().add(tftxcampoBusca);
+        getContentPane().add(lblCampoBusca);
+        getContentPane().add(lblCaminho);
+        getContentPane().add(btnresultV);
+        getContentPane().add(btnresultI);
+        getContentPane().add(btnresultA);
+        getContentPane().add(btnBusca);
+        getContentPane().add(btnUpload);
+        getContentPane().add(lbl_result);
+    }     
+    
+    public void closeAutomatoMainFrame()
+    {
+        //this.dispose();
+    }
+    @Override
+    
+    public void actionPerformed(ActionEvent evento)
+    {
+        Object objetoEvento = evento.getSource();
+        if (objetoEvento == btnBusca)
+        {
+            textoBusca = tftxcampoBusca.getText();
+            
+            if (textoBusca.isEmpty())
+            {
+                return;
+            }
+            BuscadorAutomato busca = new BuscadorAutomato();
+            try 
+            {
+                textos = textoBusca.split("[ ]");
+                tipo = "";
+                for (i=0; i<textos.length;i++)
+                {
+                    if(textos[i].equals("Imagem"))
+                        tipo = "Imagem";
+                    if(textos[i].equals("Audio"))
+                        tipo = "Audio";
+                    if(textos[i].equals("Texto"))
+                        tipo = "Texto";
+                }
+                for(i=0;i<textos.length;i++)
+                {
+                    int j=0;
+                    tipo = tipo.trim();
+                    while (j<30)
+                    {
+                        retornoBusca = busca.lerXML(textos[i], tipo, j);
+                        if (retornoBusca != null )
+                        {
+                            System.out.println("retorno busca"+ retornoBusca);
+                            caminhoFinal = retornoBusca;
+                            lblCaminho.setText("ARQUIVO ENCONTRADO");
+                        }
+                        j++;
+                    }
+                    if (caminhoFinal == "0")
+                        lblCaminho.setText("NÃO ENCONTRADO");
+                }
+            } 
+            catch (Exception ex) {
+                Logger.getLogger(MainFrameAutomato.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        if(objetoEvento == btnUpload)
+        {
+            closeAutomatoMainFrame();
+            UploadFrame upload = new UploadFrame();
+            upload.setVisible(true);
+        }
+        
+        if(objetoEvento == btnresultI)
+        {
+            lblCaminho.setText("Caminho: " + caminhoFinal);
+            try
+            {
+                    processo = Runtime.getRuntime();
+                    linhaComando = "cmd /C start " + caminhoFinal;
+                    Process exec = processo.exec(linhaComando);
+            }
+            catch (IOException ex)
+            {
+                Logger.getLogger(MainFrameAutomato.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if(objetoEvento == btnresultA)
+        {
+            lblCaminho.setText("Caminho: " + caminhoFinal);
+            try
+            {
+                    processo = Runtime.getRuntime();
+                    linhaComando = "cmd /C start " + caminhoFinal;
+                    Process exec = processo.exec(linhaComando);
+            }
+            catch (IOException ex)
+            {
+                Logger.getLogger(MainFrameAutomato.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        if(objetoEvento == btnresultV)
+        {
+            lblCaminho.setText("Caminho: " + caminhoFinal);
+            try
+            {
+                    processo = Runtime.getRuntime();
+                    linhaComando = "cmd /C start " + caminhoFinal;
+                    Process exec = processo.exec(linhaComando);
+            }
+            catch (IOException ex)
+            {
+                Logger.getLogger(MainFrameAutomato.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
---
+++ Projeto TLCF ORI - Cópia/src/Login/Login.java
@@ -0,0 +1,144 @@
+package Login;
+
+/*
+  +---------------------------------------------------------------+
+  | Pontifícia Universidade Católica de Minas Gerais - PUC Minas  |
+  | Campus de Poços de Caldas									  |
+  | Curso de Bacharelado em Ciência da Computação                 |
+  | Curso de Programação em Linguagem Java - Público Externo      |
+  | Prof. João Benedito dos Santos Junior, Ph.D.                  |
+  | Alunos: Guilherme Ricarte de Souza | Mat: 442891              |
+  |         Douglas Ravanelli Andriani  | Mat: 442888              |
+  +---------------------------------------------------------------+
+  | Login.java	                                                  |
+  | Objetivo: Desenvolver uma tela com o usuario e senha com obje-| 
+  | do usuario entrar com os dados e o sistema efetuar a validação|
+  | dos campos.                                                   |
+  +---------------------------------------------------------------+
+  */
+	
+import Utils.FilesOps;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.Image.*;
+  
+public class Login extends JFrame implements ActionListener
+{
+    
+        
+  	JTextField txt_user;
+  	JPasswordField txt_pass;
+  	JButton btn_ok, btn_cancel, btn_cadastrar;
+        JLabel lbl_user, lbl_pass, lbl_introducao, lbl_cadastrar, lbl_fundo;
+        ImageIcon img_fundo;
+        
+        Runtime processo;
+        String linhaComando;
+                        
+	public Login()
+	  {     
+     ////////////////////////////////////
+                    setBounds (400,200,600,380);
+                    setTitle("Projeto de ORI + TCLF");
+                    setResizable(false);
+                    getContentPane().setLayout(null);
+
+                    img_fundo = new ImageIcon("Files/fundo.jpg");
+                    lbl_fundo = new JLabel(img_fundo);
+                    lbl_fundo.setBounds(0,0,595,362);
+                    lbl_fundo.setLayout(null);
+    ////////////////////////////////////
+                    txt_user = new JTextField("");
+                    txt_user.setBounds(370,115,200,30);
+
+                    txt_pass = new JPasswordField("");
+                    txt_pass.setBounds(370,150,200,30);
+    ////////////////////////////////////
+                    btn_ok = new JButton("Ok");
+                    btn_ok.setBounds(520,190,50,25);
+                    btn_ok.addActionListener(this);
+                    
+                    btn_cancel = new JButton("Cancel");
+                    btn_cancel.setBounds(440,190,75,25);
+                    btn_cancel.addActionListener(this);
+
+                    btn_cadastrar = new JButton("Cadastre-se");
+                    btn_cadastrar.setBounds(460,260,105,25);
+                    btn_cadastrar.addActionListener(this);
+    ////////////////////////////////////
+                    lbl_introducao = new JLabel("Digite o usuário e senha:");
+                    lbl_introducao.setBounds(310,80,300,30);
+                    lbl_introducao.setForeground(Color.white);
+                    lbl_introducao.setFont(new Font("Arial",Font.BOLD,14));
+
+                    lbl_cadastrar = new JLabel("Primeiro login?");
+                    lbl_cadastrar.setBounds(460,230,360,30);
+                    lbl_cadastrar.setForeground(Color.white);
+                    lbl_cadastrar.setFont(new Font("Arial", Font.BOLD + Font.ITALIC, 14));
+
+                    lbl_user = new JLabel("Usuário:");
+                    lbl_user.setBounds(300,115,100,40);
+                    lbl_user.setForeground(Color.white);
+                    lbl_user.setFont(new Font("Arial",Font.BOLD,14)); 
+
+                    lbl_pass = new JLabel("Senha:");
+                    lbl_pass.setBounds(310,150,50,40);
+                    lbl_pass.setForeground(Color.white);
+                    lbl_pass.setFont(new Font("Arial",Font.BOLD,14));
+    //////////////////////////////////// 
+                    getContentPane().add(txt_user);
+                    getContentPane().add(txt_pass);
+                    getContentPane().add(btn_ok);
+                    getContentPane().add(btn_cancel);
+                    getContentPane().add(btn_cadastrar);
+                    getContentPane().add(lbl_introducao);
+                    getContentPane().add(lbl_cadastrar);
+                    getContentPane().add(lbl_user);
+                    getContentPane().add(lbl_pass);
+                    getContentPane().add(lbl_fundo);
+  	}
+       
+
+    @Override
+      	public void actionPerformed(ActionEvent evento)
+  	{
+            Object objetoEvento = evento.getSource();
+  		
+            if (objetoEvento == btn_cancel) {
+                System.exit(0);
+            }
+            if (objetoEvento == btn_ok)
+            {
+                FilesOps.escreveArquivoDisco("Files/login.txt",txt_user.getText() + "=" + txt_pass.getText()+ ";", 1);
+                try
+		{
+                    processo = Runtime.getRuntime();
+                    linhaComando = "cmd /C start cGerenciador.exe 1";
+                    processo.exec(linhaComando);
+		}	
+		catch(IOException erro)
+		{
+                    System.out.println(erro.getMessage());
+                    JOptionPane.showMessageDialog(this, "Ocorreu um erro ao abrir o arquivo cGerenciador.exe", "Aviso", JOptionPane.ERROR_MESSAGE);
+                    System.exit(1);			
+		}
+                
+                //CRIA UM DELAY PARA AGUARDA O S.O CRIAR O ARQUIVO PARA ELE LER
+                try {  
+                    Thread.sleep(2000);  
+                    verificaLogin verifica = new verificaLogin();
+                } catch (Exception e) { 
+                    e.printStackTrace();
+                }
+                
+            }
+            if (objetoEvento == btn_cadastrar)
+            {
+                JFrame cadastrar = new Cadastrar();
+                cadastrar.setVisible(true);
+            }
+            
+  	}
+  }
